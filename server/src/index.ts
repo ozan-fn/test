aa96713d202c6b7f9b981ff0daa4ff952a71af00
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2024-10-05 16:15:14 +0700

    ini

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
-import Ably from "ably";
-import helmet from "helmet";
 
 const app = express();
 
-app.use(helmet());
+// app.use(helmet());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, "../../dist")));
