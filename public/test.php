e6cfbce58b98e53704951ef4b88a0e9ed6554917
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-03-01 00:28:40 +0700

    modified public/test.php

--- public/test.php
+++ public/test.php
@@ -1,3 +1,4 @@
 <?php
-$out = shell_exec('printenv | cut -d= -f1 2>&1');
-echo '<pre>' . htmlspecialchars($out, ENT_QUOTES, 'UTF-8') . '</pre>';
+$env = array_keys($_SERVER);
+sort($env);
+echo '<pre>' . implode("\n", $env) . '</pre>';
