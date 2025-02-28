897c3a144433e7f841f34b94b86b1acee0946ab4
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-02-28 11:55:20 +0700

    s

--- public/zan6.php
+++ public/zan6.php
@@ -1,15 +1,2 @@
 <?php
-// Download composer installer
-shell_exec('php -r "copy(\'https://getcomposer.org/installer\', \'composer-setup.php\');"');
-echo "Composer installer downloaded!<br>";
-
-// Jalankan installer
-shell_exec('php composer-setup.php');
-echo "Composer installed!<br>";
-
-// Hapus file setup
-shell_exec('php -r "unlink(\'composer-setup.php\');"');
-echo "Setup file deleted!<br>";
-
-// Cek hasilnya
-echo shell_exec('php composer.phar --version');
+echo shell_exec('php composer.phar --version >2&1');
