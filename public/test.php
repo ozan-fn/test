40b8ff15914bd974fdd16b22fc822d61749af4e1
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-03-01 00:25:10 +0700

    modified public/test.php

--- public/test.php
+++ public/test.php
@@ -1,26 +1,3 @@
 <?php
-// Use getenv() to access environment variables
-$basedir = getenv('USER'); // Replace 'BASEDIR' with the actual environment variable you want to access
-$home = getenv('PWD');
-
-// Output the variables
-echo $basedir . "\n";
-echo $home . "\n";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <pre>
-        <?php echo htmlspecialchars($out, ENT_QUOTES, 'UTF-8'); ?>
-    </pre>
-</body>
-
-</html>
+echo shell_exec('echo $PWD 2>&1');
+echo shell_exec('echo $USER 2>&1');
