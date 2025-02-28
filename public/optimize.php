d21284048db120d86377f64b6f31d91b7dd5b35b
ozan-fn <23SA11A032@students.amikompurwokerto.ac.id>
2025-02-28 18:28:13 +0700

    update: optimize.php

--- public/optimize.php
+++ public/optimize.php
@@ -1,3 +1,20 @@
 <?php
-$out = shell_exec('cd .. && php -v 2>&1');
-echo $out;
+$out = shell_exec('cd .. && php artisan optimize 2>&1');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <pre>
+        <?php echo $out; ?>
+    </pre>
+</body>
+
+</html>
