8194c1f920ab662dd6c417a5b6d948373ef94ddd
AKHMAD FAUZAN <152951901+ozan-fn@users.noreply.github.com>
2026-08-12 20:15:13 +0000

    feat(server): add frankenphp proxy for phpmyadmin
    wrap phpmyadmin in frankenphp via node reverse proxy
    - spawn frankenphp binary on port 11365
    - proxy requests from port 3000 to frankenphp

---
+++ index.js
@@ -0,0 +1,25 @@
+const { createServer, request } = require('http');
+const { spawn } = require('child_process');
+const { chmodSync } = require('fs'); // Panggil modul File System
+
+const binPath = './frankenphp';
+
+// Beri izin eksekusi (setara dengan chmod +x atau chmod 755)
+chmodSync(binPath, '755');
+
+// 1. Jalankan FrankenPHP (tambahkan --root jika beda folder)
+spawn(binPath, ['php-server', '--listen', ':11365', '--root', './phpMyAdmin-5.2.3-english'], { stdio: 'inherit' });
+
+// 2. Buat Reverse Proxy Super Singkat
+createServer((req, res) => {
+    req.pipe(request({
+        hostname: '127.0.0.1',
+        port: 11365,
+        path: req.url,
+        method: req.method,
+        headers: req.headers
+    }, (proxyRes) => {
+        res.writeHead(proxyRes.statusCode, proxyRes.headers);
+        proxyRes.pipe(res);
+    })).on('error', () => res.writeHead(502).end('FrankenPHP mati.'));
+}).listen(3000, () => console.log('Web siap dibuka di http://localhost:3000\n'));
